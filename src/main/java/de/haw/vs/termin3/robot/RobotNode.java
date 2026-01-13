53c9c07c9fd4671e90fcf31e7661036b5515f175
André Maltez Nunes <andremalteznunes@sapo.pt>
2026-01-13 12:41:14 +0100

    feat: implement clean exit of robot node

--- src/main/java/de/haw/vs/termin3/robot/RobotNode.java
+++ src/main/java/de/haw/vs/termin3/robot/RobotNode.java
@@ -3,21 +3,26 @@ package de.haw.vs.termin3.robot;
 import de.haw.vs.termin3.common.json.JSONBuilder;
 import de.haw.vs.termin3.common.json.JSONReader;
 import de.haw.vs.termin3.common.network.CommunicationInterface;
-import de.haw.vs.termin3.robot.ClientHandler;
 import org.cads.vs.roboticArm.hal.ICaDSRoboticArm;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RobotNode {
     private final ICaDSRoboticArm arm;
     private final Socket registry;
     private final ServerSocket server;
+    private final List<Socket> clients;
+    private final String name;
 
     public RobotNode(String name, String registryIP, int registryPort, ICaDSRoboticArm arm) throws IOException {
+        this.name = name;
         this.arm = arm;
         this.server = new ServerSocket();
+        this.clients = new ArrayList<>();
         int port = this.server.getLocalPort();
         this.registry = new Socket(registryIP, registryPort);
         JSONBuilder builder = new JSONBuilder();
@@ -34,10 +39,23 @@ public class RobotNode {
     public void start() {
         while (true) {
             try {
-                new ClientHandler(server.accept(), arm).start();
+                Socket client = server.accept();
+                clients.add(client);
+                new ClientHandler(client, arm).start();
             } catch (IOException e) {
+                stop();
                 return;
             }
+        }
+    }
+
+    private void stop() {
+        JSONBuilder builder = new JSONBuilder();
+        builder.putString("request", "unregister");
+        builder.putString("name", name);
+        try {
+            CommunicationInterface.sendRequest(registry, builder.toString());
+        } catch (IOException _) {
         }
     }
 }
